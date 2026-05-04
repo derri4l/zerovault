12c812103c6fb6cac06c4527b69ad96ce46e5026
derri4l <dsimons0001@gmail.com>
2026-05-03 23:31:01 -0500

    check for master pass strength

--- keychain.py
+++ keychain.py
@@ -5,18 +5,11 @@ import inspect
 import json
 import logging
 import os
+import string
 
 import pyperclip
 from argon2.low_level import Type, hash_secret_raw
 from cryptography.fernet import Fernet, InvalidToken
-
-
-# logging
-def zlogger():
-    logging.basicConfig(
-        filename=LOG_FILE, level=logging.INFO, format="%(asctime)s - %(message)s"
-    )
-
 
 # variables
 SALT_FILE = "salt.bin"
@@ -43,22 +36,50 @@ def saltkey(password: str, salt: bytes) -> bytes:
     return base64.urlsafe_b64encode(raw)
 
 
+# check for master password strength with feedback
+def master_strength(masterp: str) -> bool:
+    if len(masterp) < 8:
+        print("Password too short, minimum 8 characters.")
+        return False
+    if not any(c.isupper() for c in masterp):
+        print("Password must contain an UPPERCASE letter.")
+        return False
+    if not any(c.islower() for c in masterp):
+        print("Password must contain a lowercase letter.")
+        return False
+    if not any(c.isdigit() for c in masterp):
+        print("Password must contain a number.")
+        return False
+    if not any(c in string.punctuation for c in masterp):
+        print("Password must contain a special character.")
+        return False
+    return True
+
+
 # first time vault setup
 def zsetup_vault():
-    if os.path.exists(SALT_FILE) or os.path.exists(VAULT_FILE):
-        print("Vault already exists. Delete salt.bin and vault.json to reset.")
+    if os.path.exists(VAULT_FILE):
+        print("Vault already exists. Delete vault.json to reset.")
         return
 
         # prompt for master password
-    while True:
-        password = getpass.getpass("Welcome to zerovault. Enter your master password: ")
-        confirm = getpass.getpass("Confirm your master password")
-
+    attempt = 0
+    while attempt < 3:
+        password = getpass.getpass("First time? Enter your master password: ")
+        # check for password strength
+        if not master_strength(password):
+            attempt += 1
+            continue
+        confirm = getpass.getpass("Confirm your master password: ")
         if password != confirm:
             print("Passwords don't match")
-        else:
-            print("Master password has been set successfully. Creating Vault...")
-            break
+            attempt += 1
+            continue
+        print("Master password set. Creating Vault...")
+        break
+    else:
+        print("Too many failed attempts. Exiting.")
+        exit()
 
     # generate salt.bin and write it into the vault
     salt = os.urandom(16)
@@ -84,19 +105,10 @@ def confirm_choice(max_attempts=3):
     exit()
 
 
-# setup logging for unlock_zvault
-zlogger()
-caller = inspect.stack()[0][
-    3
-]  # inspect module that called unlock_vault and logging info
-if os.path.exists(LOG_FILE):
-    os.chmod(LOG_FILE, 0o600)
-
-
 # unlock_zvault derives the key from master password to decrypt the vault
 def unlock_zvault() -> tuple[dict, Fernet, bytes]:
     attempt = 0
-    while attempt < 2:
+    while attempt < 3:
         password = getpass.getpass("Enter your master password: ")
         with open(VAULT_FILE, "rb") as v:
             data = v.read()
@@ -105,14 +117,14 @@ def unlock_zvault() -> tuple[dict, Fernet, bytes]:
         fernet = Fernet(secret_key)
         try:
             entry = json.loads(fernet.decrypt(token).decode())
-            logging.info(f"{caller} → Success:unlocked")
+
             return entry, fernet, salt
             # exit after 3 invalid attempts
         except InvalidToken:
             attempt += 1
-            logging.info(f"{caller} → Failed:unlock attempt {attempt}")
+
             print("Wrong password. Try again")
-    logging.info(f"{caller} → max attempts reached")
+
     print("Wrong password. Exiting.")
     exit()
 
@@ -127,7 +139,7 @@ def save_zvault(entry: dict, fernet: Fernet, salt: bytes):
 # zvault_add adds secrets to the vault by passing a label for it
 def zvault_add(label: str):
     entry, fernet, salt = unlock_zvault()
-
+    # check if entry already exists
     if label in entry:
         print("This label already exists")
         overwrite = input("Overwrite? (y/n): ")
@@ -161,7 +173,6 @@ def zvault_del(label: str):
     else:
         del entry[label]
 
-    logging.info(f"{caller} → {label} was deleted")
     save_zvault(entry, fernet, salt)
     print(f"'{label}' has been deleted.")
 
